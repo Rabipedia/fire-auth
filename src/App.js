2adea8e7a2822704dd2453e39a46762e7463860c
Rabiul hossain <mohammadrabiul4@gmail.com>
2021-03-31 14:03:31 -0700

    password validation added

--- src/App.js
+++ src/App.js
@@ -1,67 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import firebase from "firebase/app";
 import "firebase/auth";
-import firebaseConfig from './firebase.config';
-import { useState } from 'react';
+import firebaseConfig from "./firebase.config";
+import { useState } from "react";
 
 firebase.initializeApp(firebaseConfig);
 
 function App() {
   const [user, setUser] = useState({
     isSignedIn: false,
-    name: '',
-    email: '',
-    photo: ''
-  })
+    name: "",
+    email: "",
+    photo: "",
+  });
   const provider = new firebase.auth.GoogleAuthProvider();
   const handleSignIn = () => {
-    firebase.auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-      console.log(result)
-      const {displayName, email, photoURL} = result.user;
-      const signedInUser = {
-        isSignedIn: true,
-        name: displayName,
-        email: email,
-        photo: photoURL
-      }
-      setUser(signedInUser);
-  })
-  .catch(err => {
-    console.log(err);
-    console.log(err.message);
-  })
-  }
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+        const { displayName, email, photoURL } = result.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(signedInUser);
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log(err.message);
+      });
+  };
   const handleSignOut = () => {
-    firebase.auth().signOut()
-    .then(res => {
-      const signedOutUser = {
-        isSignedIn: false,
-        name: '',
-        photo: '',
-        email: ''
-      }
-      setUser(signedOutUser)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    firebase
+      .auth()
+      .signOut()
+      .then((res) => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: "",
+          photo: "",
+          email: "",
+        };
+        setUser(signedOutUser);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  const handleBlur = (e) => {
+    if(e.target.name === 'email'){
+      const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
+      console.log(isEmailValid);
+    }
+    if(e.target.name === 'password'){
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      console.log(isPasswordValid && passwordHasNumber);
+      
+    }
+  }
+  const handleSubmit = () => {
+
   }
   return (
     <div>
-      {
-        user.isSignedIn ?  <button onClick={handleSignOut}>Sign Out</button> :  <button onClick={handleSignIn}>Sign In</button>
-      }
-     
-      {
-        user.isSignedIn && <div>
+      {user.isSignedIn ? (
+        <button onClick={handleSignOut}>Sign Out</button>
+      ) : (
+        <button onClick={handleSignIn}>Sign In</button>
+      )}
+
+      {user.isSignedIn && (
+        <div>
           <p>WelCome, {user.name}</p>
           <p>Email: {user.email}</p>
           <img src={user.photo} alt=""></img>
         </div>
-      }
+      )}
+      <form onSubmit={handleSubmit}>
+        <input type="text" onBlur={handleBlur} name="email" placeholder="Enter Your Email" required></input>
+        <br/>
+        <input
+          type="password"
+          onBlur={handleBlur}
+          name="password"
+          placeholder="Your Password"
+          required
+        ></input>
+        <br/>
+        <input type="submit" value="Submit"/>
+      </form>
     </div>
   );
 }
